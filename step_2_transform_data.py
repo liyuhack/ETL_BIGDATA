29b19dc8aaa24eb311ba2f1014ef655a3d4e18ef
Biruk Damte <birukdamte013@gmail.com>
2025-02-13 10:30:40 +0300

    some decoration for transform.py

--- step_2_transform_data.py
+++ step_2_transform_data.py
@@ -14,13 +14,14 @@ df = spark.read.csv("./data/raw/kz.csv", header=True, inferSchema=True)
 total_rows_before = df.count()
 
 # 1. Handle Missing Values
+
 # Count the number of nulls or NaNs for each specified column
 null_counts = {}
 columns_to_check = ["event_time", "order_id", "product_id", "user_id"]
-
 for col_name in columns_to_check:
     null_counts[col_name] = df.filter(col(col_name).isNull()).count()
 
+#filter null values and nan(numeric) values with median
 df = df.dropna(subset=["event_time", "order_id", "product_id", "user_id"])
 price_median = df.approxQuantile("price", [0.5], 0.05)[0]
 df = df.fillna({"price": price_median})
@@ -32,9 +33,6 @@ df = df = df.dropDuplicates(subset=["order_id", "product_id"])  # cleaning rows
 # 3. Validate User IDs
 df = df.filter(col("user_id").cast("int").isNotNull() & (col("user_id") > 0))
 
-# # 3. Format Timestamps
-# df = df.withColumn("event_time", to_timestamp("event_time"))
-
 # 4. Add a new column for the day of the week
 df = df.withColumn("day_of_week", date_format(col("event_time"), "E"))
 
@@ -42,19 +40,7 @@ df = df.withColumn("day_of_week", date_format(col("event_time"), "E"))
 df = df.withColumn("category_code", trim(lower(col("category_code"))))
 df = df.withColumn("brand", trim(lower(col("brand"))))
 
-# # 5. Handle Outliers in Prices
-# Q1, Q3 = df.approxQuantile("price", [0.25, 0.75], 0.05)
-# IQR = Q3 - Q1
-# lower_bound = Q1 - 1.5 * IQR
-# upper_bound = Q3 + 1.5 * IQR
-# noised_data_before = df.filter((col("price") < lower_bound) | (col("price") > upper_bound)).count()
-# print(f"Noised Data Count Before Cleaning: {noised_data_before}")
-
-# df = df.filter((col("price") >= lower_bound) & (col("price") <= upper_bound))
-# noised_data_after = df.filter((col("price") < lower_bound) | (col("price") > upper_bound)).count()
-# print(f"Noised Data Count After Cleaning: {noised_data_after}")
-
-# 6. Encode Categories
+# 6. Encode Categories with freindly name for analysis
 df = df.withColumn(
     "category_encoded", 
     when(col("category_code").startswith("electronics"),"electronics")
@@ -66,18 +52,19 @@ df = df.withColumn(
     .otherwise("others")  # Default value for non-matching categories
 )
 
-# Count rows after cleaning
+# Counting after cleaning
+print("\n--------------------------------------------------------------")
 total_rows_after = df.count() 
 print(f"Total Rows Before Cleaning: {total_rows_before}")
 for column, count in null_counts.items():
-    print(f"Number of nulls before cleaning in {column}: {count}")
+    print(f"Number of nulls before cleaning in {column} column: {count}")
 print(f"Duplicate Count Before Cleaning: {duplicate_count_before}")
 print(f"Total Rows After cleaing: {total_rows_after}")
+print("\n--------------------------------------------------------------")
 
 # Save Cleaned Dataset with Specific Filename
 output_dir = "./data/cleaned/"
 output_file = "cleaned_electronics_data.csv"
-
 df.coalesce(1).write.csv(output_dir, header=True, mode="overwrite")
 
 # Rename the single part file to the desired filename
