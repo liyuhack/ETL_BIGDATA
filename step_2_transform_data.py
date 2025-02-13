ac0a604b5fa3a4c07c6d125dff3bea5a1c8fc02b
Biruk Damte <birukdamte013@gmail.com>
2025-02-13 10:23:06 +0300

    cleaning code optimized prints and corrected

--- step_2_transform_data.py
+++ step_2_transform_data.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, mean, when, count, trim, lower, to_timestamp
+from pyspark.sql.functions import col, mean, when, count, trim, lower, to_timestamp, isnan
 from pyspark.sql.types import FloatType
+from pyspark.sql.functions import date_format
 import os
 
 # Initialize Spark Session
@@ -9,39 +10,49 @@ spark = SparkSession.builder.appName("ElectronicsDataCleaning").getOrCreate()
 # Load Dataset
 df = spark.read.csv("./data/raw/kz.csv", header=True, inferSchema=True)
 
+# Count rows before cleaning
+total_rows_before = df.count()
+
 # 1. Handle Missing Values
+# Count the number of nulls or NaNs for each specified column
+null_counts = {}
+columns_to_check = ["event_time", "order_id", "product_id", "user_id"]
+
+for col_name in columns_to_check:
+    null_counts[col_name] = df.filter(col(col_name).isNull()).count()
+
 df = df.dropna(subset=["event_time", "order_id", "product_id", "user_id"])
 price_median = df.approxQuantile("price", [0.5], 0.05)[0]
 df = df.fillna({"price": price_median})
 
-# 2. Format Timestamps
-df = df.withColumn("event_time", to_timestamp("event_time"))
-
-# 3. Remove Duplicates
-total_rows_before = df.count() # Count rows before cleaning
+# 2. Remove Duplicates
 duplicate_count_before = df.count() - df.dropDuplicates(subset=["order_id", "product_id"]).count() # Count duplicates before cleaning
-df = df.dropDuplicates(subset=["order_id", "product_id"])  # cleaning rows
-total_rows_after = df.count() # Count rows after cleaning
-print(f"Total Rows Before Cleaning: {total_rows_before}")
-print(f"Duplicate Count Before Cleaning: {duplicate_count_before}")
-print(f"Total Rows After Removing Duplicates: {total_rows_after}")
+df = df = df.dropDuplicates(subset=["order_id", "product_id"])  # cleaning rows
 
+# 3. Validate User IDs
+df = df.filter(col("user_id").cast("int").isNotNull() & (col("user_id") > 0))
 
-# 4. Standardize Text Fields
+# # 3. Format Timestamps
+# df = df.withColumn("event_time", to_timestamp("event_time"))
+
+# 4. Add a new column for the day of the week
+df = df.withColumn("day_of_week", date_format(col("event_time"), "E"))
+
+# 5. Standardize Text Fields
 df = df.withColumn("category_code", trim(lower(col("category_code"))))
 df = df.withColumn("brand", trim(lower(col("brand"))))
 
-# 5. Handle Outliers in Prices
-Q1, Q3 = df.approxQuantile("price", [0.25, 0.75], 0.05)
-IQR = Q3 - Q1
-lower_bound = Q1 - 1.5 * IQR
-upper_bound = Q3 + 1.5 * IQR
-noised_data_before = df.filter((col("price") < lower_bound) | (col("price") > upper_bound)).count()
-print(f"Noised Data Count Before Cleaning: {noised_data_before}")
+# # 5. Handle Outliers in Prices
+# Q1, Q3 = df.approxQuantile("price", [0.25, 0.75], 0.05)
+# IQR = Q3 - Q1
+# lower_bound = Q1 - 1.5 * IQR
+# upper_bound = Q3 + 1.5 * IQR
+# noised_data_before = df.filter((col("price") < lower_bound) | (col("price") > upper_bound)).count()
+# print(f"Noised Data Count Before Cleaning: {noised_data_before}")
 
-df = df.filter((col("price") >= lower_bound) & (col("price") <= upper_bound))
-noised_data_after = df.filter((col("price") < lower_bound) | (col("price") > upper_bound)).count()
-print(f"Noised Data Count After Cleaning: {noised_data_after}")
+# df = df.filter((col("price") >= lower_bound) & (col("price") <= upper_bound))
+# noised_data_after = df.filter((col("price") < lower_bound) | (col("price") > upper_bound)).count()
+# print(f"Noised Data Count After Cleaning: {noised_data_after}")
 
 # 6. Encode Categories
 df = df.withColumn(
@@ -55,12 +66,13 @@ df = df.withColumn(
     .otherwise("others")  # Default value for non-matching categories
 )
 
-
-# 7. Filter Relevant Categories
-# df = df.filter(col("category_code").contains("electronics"))
-
-# 8. Validate User IDs
-df = df.filter(col("user_id").cast("int").isNotNull() & (col("user_id") > 0))
+# Count rows after cleaning
+total_rows_after = df.count() 
+print(f"Total Rows Before Cleaning: {total_rows_before}")
+for column, count in null_counts.items():
+    print(f"Number of nulls before cleaning in {column}: {count}")
+print(f"Duplicate Count Before Cleaning: {duplicate_count_before}")
+print(f"Total Rows After cleaing: {total_rows_after}")
 
 # Save Cleaned Dataset with Specific Filename
 output_dir = "./data/cleaned/"
